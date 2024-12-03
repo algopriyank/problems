4061a72af233f7cf4e31c23ea15bb21ad0943f35
Priyank Sharma <algo.priyank@gmail.com>
2024-12-03 10:45:56 +0530

    modified palindrome

--- plaindrome.cpp
+++ plaindrome.cpp
@@ -1,21 +1,20 @@
-/*
-Identifying Palindromes
-Difficulty: Easy
-Topics: Basic Programming, String Manipulation
-Description: Write a program to check if a string or number is a palindrome.
-Example:
-Input: string = "radar"
-Output: Palindrome
-Explanation: "radar" reads the same backward as forward.
-*/
-
-void check_palindrome() {
-
-}
-
 #include<iostream>
 using namespace std;
 
+void check_palindrome() {
+    string word;
+    cout << "enter the word to check for palindrome: ";
+    cin >> word;
+    
+    //check if word's first char is same as last and repeat the process until the middle of the string
+
+    for (int i=0; i<word.length()/2; i++)   {
+        if (word[i] != word[word.length() -i - 1]) {
+            cout << word << " -> is not a palindrome" << endl;
+        }
+        else cout << word << " -> is a palindrome" << endl;
+    }
+}
 int main()
 {
     check_palindrome();
